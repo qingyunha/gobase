8e06eb7dbcc73cd88250737a36e983f0747ceae0
qingyunha <845767657@qq.com>
2020-05-09 10:49:23 +0800

    glimit: update

--- glimit/glimit.go
+++ glimit/glimit.go
@@ -2,6 +2,10 @@
 
 package glimit
 
+import (
+	"reflect"
+)
+
 type Glimit struct {
 	n int
 	c chan struct{}
@@ -21,4 +25,22 @@ func (g *Glimit) Run(f func()) {
 		f()
 		<-g.c
 	}()
+}
+
+// A convenient version of Run
+func (g *Glimit) Runf(f interface{}, args ...interface{}) {
+	g.c <- struct{}{}
+	go func() {
+		call(f, args...)
+		<-g.c
+	}()
+}
+
+func call(f interface{}, args ...interface{}) {
+	fv := reflect.ValueOf(f)
+	in := make([]reflect.Value, len(args))
+	for i := range args {
+		in[i] = reflect.ValueOf(args[i])
+	}
+	fv.Call(in)
 }
--- glimit/glimit_test.go
+++ glimit/glimit_test.go
@@ -36,3 +36,16 @@ func TestGlimit(t *testing.T) {
 		t.Errorf("c[3] should be true")
 	}
 }
+
+func TestRunf(t *testing.T) {
+	gl := New(1)
+	var c int
+	f := func(a int) { c = c + a }
+	gl.Runf(f, 1)
+	gl.Runf(f, 1)
+	gl.Runf(f, 1)
+	time.Sleep(2 * time.Millisecond)
+	if c != 3 {
+		t.Error("c should eqaul 3 ", c)
+	}
+}
